a994c695b1e362a6198fcc4368fcc2ef58362bcb
Ilan <ilan@chemla.fr>
2018-06-28 23:13:44 +0300

    Generate GUI refactor

--- gui/generate_gui.py
+++ gui/generate_gui.py
@@ -2,37 +2,29 @@ import datetime
 from string import Template
 
 import click
-import requests
-import json
 
 from config.config import cfg
-
-
-def get_flag_url(team_link):
-    response_ = requests.get(team_link)
-    response_ = json.loads(response_.text)
-    return response_['crestUrl']
+from utils import Games
 
 
 @click.command()
 @click.option('--match-day', required=True, help='Round\'s number', type=int)
 def generate_gui(match_day):
     print('Generating bets table for round {}...'.format(match_day))
-    # Get fixtures
-    contents = requests.get('http://api.football-data.org/v1/competitions/467/fixtures')
-    contents = json.loads(contents.text)
 
+    # Get fixtures
+    fixtures = Games.get_fixtures()
     games_divs = []
     date_format = "%Y-%m-%dT%H:%M:%SZ"
-    for ids, game in enumerate(contents['fixtures']):
+    for ids, game in enumerate(fixtures):
         if game['matchday'] != match_day:
             continue
 
         date_obj = datetime.datetime.strptime(game['date'], date_format)
         date_obj += datetime.timedelta(hours=3)
 
-        homeTeamFlag = '<img src={} width=32/>'.format(get_flag_url(game['_links']['homeTeam']['href']))
-        awayTeamFlag = '<img src={} width=32/>'.format(get_flag_url(game['_links']['awayTeam']['href']))
+        homeTeamFlag = '<img src={} width=32/>'.format(Games.get_team_flag(game['homeTeamId']))
+        awayTeamFlag = '<img src={} width=32/>'.format(Games.get_team_flag(game['awayTeamId']))
 
         div = '<tr>'
         div += "<td>Match {}</td><td>{} {} - {} {}</td>".format(ids, homeTeamFlag, game['homeTeamName'], game['awayTeamName'], awayTeamFlag)
@@ -42,7 +34,8 @@ def generate_gui(match_day):
         div += '<option selected value> -- select your bet -- </option>'
         div += '<option value="1">{} wins (1)</option>'.format(game['homeTeamName'])
         div += '<option value="2">{} wins (2)</option>'.format(game['awayTeamName'])
-        div += '<option value="X">Draw (X)</option>'
+        if match_day < 4:
+            div += '<option value="X">Draw (X)</option>'
         div += '</select>'
         div += '</td>'
         div += '</tr>'
@@ -61,8 +54,8 @@ def generate_gui(match_day):
     # do the substitution
     result = src.substitute(d)
 
-    output_file = "dist/bets_{}.html"
-    file = open(output_file.format(match_day), "w")
+    output_file = "dist/bets_{}.html".format(match_day)
+    file = open(output_file, "w")
     file.write(result)
     file.close()
     print('Done. Output path: {}'.format(output_file))
